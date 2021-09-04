8817352aafa507e0591374f10034b7f9d50ff0e3
vitaminka-007 <pavelhrytsuk@magedia.com>
2021-09-04 15:05:18 +0300

    qwe

---
+++ app/code/PavelHr/ModalPopUp/view/frontend/web/js/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            '*': {
+                'PavelHr_ModalPopUp/js/custom-request/last-added-to-cart-mixin': true
+            }
+        }
+    }
+};
