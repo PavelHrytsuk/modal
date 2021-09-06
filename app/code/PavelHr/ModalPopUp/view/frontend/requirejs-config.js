9c9ee49faeb48064a5d0777807af793d577f8553
vitaminka-007 <pavelhrytsuk@magedia.com>
2021-09-06 16:10:56 +0300

    Ajax

--- app/code/PavelHr/ModalPopUp/view/frontend/requirejs-config.js
+++ app/code/PavelHr/ModalPopUp/view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     config: {
         mixins: {
-            '*': {
+            'Magento_Catalog/js/catalog-add-to-cart': {
                 'PavelHr_ModalPopUp/js/custom-request/last-added-to-cart-mixin': true
             }
         }
